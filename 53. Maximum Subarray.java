97aa43e27ce63dea66258f44bdf6f055afb9901c
Gourav Soni <32902088+the-garv@users.noreply.github.com>
2022-08-23 08:06:12 +0530

    Updated indentation

--- 53. Maximum Subarray.java
+++ 53. Maximum Subarray.java
@@ -3,12 +3,8 @@ class Solution {
         int resultSum=Integer.MIN_VALUE, currentSum = 0;
         for(int i=0;i<nums.length;i++){
             currentSum =currentSum + nums[i];
-            if(currentSum > resultSum){
-                resultSum = currentSum;
-            }
-            if(currentSum < 0){
-                currentSum = 0;
-            }
+            if(currentSum > resultSum)  resultSum = currentSum;
+            if(currentSum < 0)  currentSum = 0;
         }
         return resultSum;
     }
