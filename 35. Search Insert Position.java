26493eaaf07194dd685317aa47842190ff37f754
Gourav Soni <32902088+the-garv@users.noreply.github.com>
2022-08-23 23:03:27 +0530

    O(log n) T O(1) - M

---
+++ 35. Search Insert Position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i=0,j=nums.length;
+        while(i<j){
+            int mid = (i+j)/2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] > target)    j = mid;
+            else i = mid+1;
+        }
+        return j;
+    }
+    
+}
