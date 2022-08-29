c1c72a4fe5ab08127abe3a9530b8ba9a1d3459ef
Gourav Soni <32902088+the-garv@users.noreply.github.com>
2022-08-30 02:00:27 +0530

    variable name change

--- 217. Contains Duplicate.java
+++ 217. Contains Duplicate.java
@@ -1,10 +1,10 @@
 import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Hashtable<Integer, Integer> hs = new Hashtable<Integer, Integer>();
+        Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
         for(int i=0;i<nums.length;i++){
-            if(hs.containsKey(nums[i]))    return true;
-            hs.put(nums[i], 0);
+            if(ht.containsKey(nums[i]))    return true;
+            ht.put(nums[i], 0);
         }
         return false;
     }
